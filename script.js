2288a94455f819dfa2a1294a213692af2db9509b
sonukuldeep <sonukuldeep@outlook.com>
2022-09-20 09:08:27 +0530

    bug fix and blog element completed

--- script.js
+++ script.js
@@ -19,10 +19,24 @@ const cards = document.querySelectorAll('.card');
 const observer = new IntersectionObserver(entries=>{
   entries.forEach(entry=>{
     entry.target.classList.toggle('show',entry.isIntersecting)
-    console.log(entry.target.classList,entry.isIntersecting)
+
+    // console.log(entry.target.classList,entry.isIntersecting)
   })
-},{threshold: 0.5})
+},{threshold: 0.5,})
 
 cards.forEach(card=>{
   observer.observe(card)
+})
+
+const cards2 = document.querySelectorAll('.card2');
+const observer2 = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    entry.target.classList.toggle('show',entry.isIntersecting)
+    if(entry.isIntersecting) observer2.unobserve(entry.target)
+    // console.log(entry.target.classList,entry.isIntersecting)
+  })
+},{threshold: 0.2,})
+
+cards2.forEach(card=>{
+  observer2.observe(card)
 })
