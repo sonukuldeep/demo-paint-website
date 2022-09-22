396ab8121c9c66e382283744a86fb7c011d91488
sonukuldeep <sonukuldeep@outlook.com>
2022-09-22 22:22:53 +0530

    carousel in progress

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
 
 
-let counter = 0;
-let smartCount = 1;
-setInterval(() => {
-    counter +=smartCount;
-    const wp = document.getElementById('radio'+counter).checked = true;
+// let counter = 0;
+// let smartCount = 1;
+// setInterval(() => {
+//     counter +=smartCount;
+//     const wp = document.getElementById('radio'+counter).checked = true;
     
-    // console.log(counter)
-    if(counter === 7) smartCount = -1
-    if(counter === 1) smartCount = 1
+//     // console.log(counter)
+//     if(counter === 7) smartCount = -1
+//     if(counter === 1) smartCount = 1
 
-}, 5000);
+// }, 5000);
 
+// carousel start
+// getting important classes
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const dotsNav = document.querySelector(".carousel__nav");
+const dots = Array.from(dotsNav.children);
+// nextSlide variable stores clicked position to resume auto scroll from
+let nextSlide = 0;
+
+// initially positioning all slides horizentally
+slides.forEach((slide, index) => {
+  slide.style.left = slide.getBoundingClientRect().width * index + "px";
+});
+
+// remove currentSlide class on old slide and adding the same to a new one
+const setCurrentSlideClass = (index) => {
+  const currentSlide = dotsNav.querySelector('.current-slide')
+  currentSlide.classList.remove('current-slide')
+  dots[index].classList.add('current-slide')
+  nextSlide = index;
+}
+
+// main code that does the shifting and moves classes
+const shiftSlide = index =>{
+  setCurrentSlideClass(index)
+  const amountToMove = slides[index].style.left;
+  track.style.transform = 'translateX(-' + amountToMove + ')';
+}
+
+// adding event listners to nav buttons
+dots.forEach((dot,index) => {
+  dot.addEventListener('click', e=> {shiftSlide(index)})
+})
+
+// this code below makes automatic scroll function
+const numberOfSlides = slides.length
+let posOrNeg = 1;
+setInterval(()=>{
+  shiftSlide(nextSlide)
+  if(nextSlide === numberOfSlides -1) posOrNeg = -1
+  if( nextSlide === 0) posOrNeg = 1
+  nextSlide +=posOrNeg
+},3000)
+// carousel end
 
 //animatedGrid
 
